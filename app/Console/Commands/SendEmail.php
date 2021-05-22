a622135bd9b8f3e52f056934da41ce9083fb9489
osama <osamaaroshdy@gmail.com>
2021-05-22 02:36:09 +0200

    a console command that sends an email every hour

---
+++ app/Console/Commands/SendEmail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\SubscribeEmailCount;
+use App\Models\SubscribeEmail;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send email every hour ';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $count = SubscribeEmail::count();
+        return Mail::to('osamaaroshdy@gmail.com')->send(new SubscribeEmailCount($count));
+    }
+}
